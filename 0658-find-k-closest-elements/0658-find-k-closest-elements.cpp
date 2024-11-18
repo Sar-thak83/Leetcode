43fa0f5583f84e82f6832a6f8c064691918cdb43
Sarthak Sahu <sarthak8385@gmail.com>
2024-11-18 17:02:40 +0530

    Time: 3 ms (58.06%), Space: 34.3 MB (74.02%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,21 +1,50 @@
+
+// ----------------Approach - 1----------------
+
+// class Solution {
+// public:
+    
+//     priority_queue<pair<int, int>>pq;
+//     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+//         for(auto ele:arr)
+//         {
+//             pq.push({abs(x-ele),ele});
+//             if(pq.size()>k)
+//             pq.pop();
+//         }
+//         vector<int>ans;
+//         while(!pq.empty())
+//         {
+//             ans.push_back(pq.top().second);
+//             pq.pop();
+//         }
+//         sort(ans.begin(),ans.end());
+//         return ans ;
+//     }
+// };
+
+
+
 class Solution {
 public:
-    
-    priority_queue<pair<int, int>>pq;
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        for(auto ele:arr)
-        {
-            pq.push({abs(x-ele),ele});
-            if(pq.size()>k)
-            pq.pop();
+        int low=0;
+        int high=arr.size()-1;
+
+        while(high-low>=k){
+            if(x-arr[low]>arr[high]-x){
+                low++;
+            }
+            else{
+                high--;
+            }
         }
-        vector<int>ans;
-        while(!pq.empty())
-        {
-            ans.push_back(pq.top().second);
-            pq.pop();
+
+        vector<int> ans;
+        for(int i=low;i<=high;i++){
+
+            ans.push_back(arr[i]);
         }
-        sort(ans.begin(),ans.end());
-        return ans ;
+        return ans;
     }
 };
