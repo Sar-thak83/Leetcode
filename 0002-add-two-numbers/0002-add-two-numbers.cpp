61686e411c35c578cc49b0e3a69fab2d8554c44d
Sarthak  Sahu <sarthak8385@gmail.com>
2025-07-06 13:31:29 +0530

    Time: 0 ms (100%), Space: 77 MB (91.29%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -8,53 +8,53 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-// class Solution {
-// public:
-//     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-//         ListNode* dummy = new ListNode();
-//         ListNode* temp = dummy;
-//         int carry = 0;
-//         while (l1 != NULL || l2 != NULL || carry) {
-//             int sum = 0;
-//             if (l1 != NULL) {
-//                 sum += l1->val;
-//                 l1 = l1->next;
-//             }
-//             if (l2 != NULL) {
-//                 sum += l2->val;
-//                 l2 = l2->next;
-//             }
-//             sum += carry;
-//             carry = sum / 10;
-//             ListNode* newnode = new ListNode(sum % 10);
-//             temp->next = newnode;
-//             temp = temp->next;
-//         }
-//         return dummy->next;
-//     }
-// };
-
 class Solution {
 public:
-    ListNode* add(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode(0); 
-        ListNode* curr = dummy;
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode();
+        ListNode* temp = dummy;
         int carry = 0;
-
-        while (l1 != NULL || l2 != NULL || carry != 0) {
-            int x = (l1 != NULL) ? l1->val : 0; 
-            int y = (l2 != NULL) ? l2->val : 0; 
-            int sum =x+y+carry;
-            carry=sum/10;
-            curr->next=new ListNode(sum%10);
-            curr=curr->next;
-            if(l1!=NULL) l1=l1->next;
-            if(l2!=NULL) l2=l2->next;
+        while (l1 != NULL || l2 != NULL || carry) {
+            int sum = 0;
+            if (l1 != NULL) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2 != NULL) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            sum += carry;
+            carry = sum / 10;
+            ListNode* newnode = new ListNode(sum % 10);
+            temp->next = newnode;
+            temp = temp->next;
         }
         return dummy->next;
     }
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* ans = add(l1, l2);
-        return ans;
-    }
 };
+
+// class Solution {
+// public:
+//     ListNode* add(ListNode* l1, ListNode* l2) {
+//         ListNode* dummy = new ListNode(0); 
+//         ListNode* curr = dummy;
+//         int carry = 0;
+
+//         while (l1 != NULL || l2 != NULL || carry != 0) {
+//             int x = (l1 != NULL) ? l1->val : 0; 
+//             int y = (l2 != NULL) ? l2->val : 0; 
+//             int sum =x+y+carry;
+//             carry=sum/10;
+//             curr->next=new ListNode(sum%10);
+//             curr=curr->next;
+//             if(l1!=NULL) l1=l1->next;
+//             if(l2!=NULL) l2=l2->next;
+//         }
+//         return dummy->next;
+//     }
+//     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+//         ListNode* ans = add(l1, l2);
+//         return ans;
+//     }
+// };
