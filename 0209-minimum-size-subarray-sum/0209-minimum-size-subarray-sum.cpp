8963335c9dd6503c359bca8ea962a26977423a33
Sarthak  Sahu <sarthak8385@gmail.com>
2025-08-18 18:48:50 +0530

    Time: 3 ms (13.04%), Space: 32.2 MB (6.51%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,44 +1,40 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n = nums.size();
+        int left = 0, sum = 0;
+        int minLen = INT_MAX;
+        for (int right = 0; right < n; right++) {
+            sum += nums[right];
+            while (sum >= target) {
+                minLen = min(minLen, right - left + 1);
+                sum -= nums[left];
+                left++;
+            }
+        }
+        return (minLen == INT_MAX) ? 0 : minLen;
+    }
+};
+
+// -------getting TLE not optimized--------------
+
 // class Solution {
 // public:
 //     int minSubArrayLen(int target, vector<int>& nums) {
 //         int n = nums.size();
-//         int left = 0, sum = 0;
 //         int minLen = INT_MAX;
+//         for (int i = 0; i < n; i++) {
+//             int sum = 0;
+//             for (int j = i; j < n; j++) {
+//                 sum += nums[j];  
 
-//         for (int right = 0; right < n; right++) {
-//             sum += nums[right];
-
-//             // shrink the window while the condition is satisfied
-//             while (sum >= target) {
-//                 minLen = min(minLen, right - left + 1);
-//                 sum -= nums[left];
-//                 left++;
+//                 if (sum >= target) {
+//                     minLen = min(minLen, j - i + 1);
+//                     break; 
+//                 }
 //             }
 //         }
 
 //         return (minLen == INT_MAX) ? 0 : minLen;
 //     }
 // };
-
-
-
-class Solution {
-public:
-    int minSubArrayLen(int target, vector<int>& nums) {
-        int n = nums.size();
-        int minLen = INT_MAX;
-        for (int i = 0; i < n; i++) {
-            int sum = 0;
-            for (int j = i; j < n; j++) {
-                sum += nums[j];  
-
-                if (sum >= target) {
-                    minLen = min(minLen, j - i + 1);
-                    break; 
-                }
-            }
-        }
-
-        return (minLen == INT_MAX) ? 0 : minLen;
-    }
-};
