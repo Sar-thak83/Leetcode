42fff4f578536e8e33f2d1c5984b4ea253a83413
Sarthak  Sahu <sarthak8385@gmail.com>
2025-08-19 19:26:11 +0530

    Time: 297 ms (5.04%), Space: 22.8 MB (99.36%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        int n = strs.size();
+        vector<string> temp(n);
+        vector<vector<string>> ans;
+        vector<bool> used(n, false);
+        
+        for (int i = 0; i < n; i++) {
+            string s = strs[i];
+            sort(s.begin(), s.end());
+            temp[i] = s;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (used[i])
+                continue;
+            vector<string> group;
+            group.push_back(strs[i]);
+            used[i] = true;
+            for (int j = i + 1; j < n; j++) {
+                if (!used[j] && temp[i] == temp[j]) {
+                    group.push_back(strs[j]);
+                    used[j] = true;
+                }
+            }
+            ans.push_back(group);
+        }
+
+        return ans;
+    }
+};
