519e2152e2bd42db4ee886c644583913749b3b06
Sarthak  Sahu <sarthak8385@gmail.com>
2025-08-19 19:45:35 +0530

    Time: 10 ms (93.66%), Space: 23.9 MB (94.44%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,32 +1,53 @@
+// class Solution {
+// public:
+//     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+//         int n = strs.size();
+//         vector<string> temp(n);
+//         vector<vector<string>> ans;
+//         vector<bool> used(n, false);
+        
+//         for (int i = 0; i < n; i++) {
+//             string s = strs[i];
+//             sort(s.begin(), s.end());
+//             temp[i] = s;
+//         }
+
+//         for (int i = 0; i < n; i++) {
+//             if (used[i])
+//                 continue;
+//             vector<string> group;
+//             group.push_back(strs[i]);
+//             used[i] = true;
+//             for (int j = i + 1; j < n; j++) {
+//                 if (!used[j] && temp[i] == temp[j]) {
+//                     group.push_back(strs[j]);
+//                     used[j] = true;
+//                 }
+//             }
+//             ans.push_back(group);
+//         }
+//         return ans;
+//     }
+// };
+
+
+
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        int n = strs.size();
-        vector<string> temp(n);
-        vector<vector<string>> ans;
-        vector<bool> used(n, false);
+        unordered_map<string, vector<string>> groups;
         
-        for (int i = 0; i < n; i++) {
-            string s = strs[i];
-            sort(s.begin(), s.end());
-            temp[i] = s;
+        for (string& s : strs) {
+            string sorted_s = s;
+            sort(sorted_s.begin(), sorted_s.end());
+            groups[sorted_s].push_back(s);
         }
-
-        for (int i = 0; i < n; i++) {
-            if (used[i])
-                continue;
-            vector<string> group;
-            group.push_back(strs[i]);
-            used[i] = true;
-            for (int j = i + 1; j < n; j++) {
-                if (!used[j] && temp[i] == temp[j]) {
-                    group.push_back(strs[j]);
-                    used[j] = true;
-                }
-            }
-            ans.push_back(group);
+        
+        vector<vector<string>> ans;
+        for (auto& kv : groups) {
+            ans.push_back(move(kv.second));
         }
-
+        
         return ans;
     }
-};
+}; 
