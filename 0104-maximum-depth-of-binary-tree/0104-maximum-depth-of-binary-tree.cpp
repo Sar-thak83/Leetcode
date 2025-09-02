c4c2ea319d5423b0b45639c11a1d361a7eff690d
Sarthak  Sahu <sarthak8385@gmail.com>
2025-09-02 17:14:37 +0530

    Time: 0 ms (100%), Space: 18.8 MB (95.36%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -1,38 +1,38 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// class Solution {
+// public:
+//     int maxDepth(TreeNode* root) {
+//         if(root==NULL)
+//         {
+//             return 0;
+//         }
+//         queue <TreeNode*> q;
+//         q.push(root);
+//         int height=0;
+//         while(!q.empty())
+//         {
+//             int size = q.size();
+//             vector<int>level;
+//             for(int i=0;i<size;i++)
+//             {
+//                 TreeNode *node=q.front();
+//                 q.pop();
+//                 level.push_back(node->val);
+//                 if(node->right!=NULL)q.push(node->right);
+//                 if(node->left!=NULL)q.push(node->left);
+//             }
+//             height++;
+//         }
+//         return height;
+//     }
+// };
+
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        if(root==NULL)
-        {
+        if (root == NULL)
             return 0;
-        }
-        queue <TreeNode*> q;
-        q.push(root);
-        int height=0;
-        while(!q.empty())
-        {
-            int size = q.size();
-            vector<int>level;
-            for(int i=0;i<size;i++)
-            {
-                TreeNode *node=q.front();
-                q.pop();
-                level.push_back(node->val);
-                if(node->right!=NULL)q.push(node->right);
-                if(node->left!=NULL)q.push(node->left);
-            }
-            height++;
-        }
-        return height;
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+        return 1 + max(left, right);
     }
 };
