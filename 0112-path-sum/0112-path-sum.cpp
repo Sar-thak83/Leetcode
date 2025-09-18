878018bf77744b6f61b280f5fa425994052d6992
Sarthak  Sahu <sarthak8385@gmail.com>
2025-09-18 19:52:41 +0530

    Time: 2 ms (5.03%), Space: 21.8 MB (5.66%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -10,15 +10,42 @@
  * right(right) {}
  * };
  */
+
+// -----Recursive Approach-----------
+
+// class Solution {
+// public:
+//     bool hasPathSum(TreeNode* root, int targetSum) {
+//         if (!root)
+//             return false;
+//         if (!root->left && !root->right)
+//             return root->val == targetSum;
+//         bool left = hasPathSum(root->left, targetSum - root->val);
+//         bool right = hasPathSum(root->right, targetSum - root->val);
+//         return left || right;
+//     }
+// };
+
+// -------BFS iterative Approach-----------
+
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
         if (!root)
             return false;
-        if (!root->left && !root->right)
-            return root->val == targetSum;
-        bool left = hasPathSum(root->left, targetSum - root->val); 
-        bool right= hasPathSum(root->right,targetSum - root->val);
-        return left|| right;
+
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, root->val});
+        while (!q.empty()) {
+            auto [node, currSum] = q.front();
+            q.pop();
+            if (!node->left && !node->right && currSum == targetSum)
+                return true;
+            if (node->left)
+                q.push({node->left, currSum + node->left->val});
+            if (node->right)
+                q.push({node->right, currSum + node->right->val});
+        }
+        return false;
     }
 };
