aaefbff3a6335408441434f56cb1d8a31133899f
Sarthak  Sahu <sarthak8385@gmail.com>
2025-10-04 18:37:22 +0530

    Time: 0 ms (100%), Space: 63 MB (48.17%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,3 +1,27 @@
+// class Solution {
+// public:
+//     int maxArea(vector<int>& height) {
+//         int low = 0;
+//         int high = height.size() - 1;
+//         int ans = 0;
+//         while (low < high) {
+//             int temp;
+//             if (height[low] > height[high]) {
+//                 temp=height[high] * (high-low);
+//                 ans = max(temp, ans);
+//                 high--;
+//             }
+//             else{
+//                 temp=height[low] * (high-low);
+//                 ans = max(temp, ans);
+//                 low++;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     int maxArea(vector<int>& height) {
@@ -5,18 +29,15 @@ public:
         int high = height.size() - 1;
         int ans = 0;
         while (low < high) {
-            int temp;
-            if (height[low] > height[high]) {
-                temp=height[high] * (high-low);
-                ans = max(temp, ans);
+            int temp = min(height[low], height[high]) * (high - low);
+            ans = max(ans, temp);
+            if (height[low] < height[high]) {
+                low++;
+            } else {
                 high--;
             }
-            else{
-                temp=height[low] * (high-low);
-                ans = max(temp, ans);
-                low++;
-            }
         }
+
         return ans;
     }
 };
